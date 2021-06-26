54a83eaa614e0e686e2fa6703a845809259705fd
Samvith-Tallogu <77870783+Samvith-Tallogu@users.noreply.github.com>
2021-06-26 15:06:40 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,14 @@
+function preload(){}
+
+function setup(){
+    canvas = createCanvas(350 , 300);
+    canvas.center();
+}
+
+function draw(){
+
+}
+
+function take_snapshot() {
+    save("me_with_clown_nose.png");
+}
