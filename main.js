9a9fbeb3c1f4230a6be1faa3e4d2d78d5292fab9
Samvith-Tallogu <77870783+Samvith-Tallogu@users.noreply.github.com>
2021-06-29 08:39:49 +0530

    Part 2 Completed

--- main.js
+++ main.js
@@ -3,10 +3,25 @@ function preload(){}
 function setup(){
     canvas = createCanvas(350 , 300);
     canvas.center();
+    video = createCapture(VIDEO);
+    video.size(350, 300);
+    video.hide();
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+function modelLoaded() {
+    console.log("Model Loaded");
+}
+function gotPoses(results) {
+    if (results.length > 0) {
+        console.log(results);
+        console.log('nose X = '+ results[0].pose.nose.x);
+        console.log('nose Y = '+ results[0].pose.nose.y);
+    }
 }
 
 function draw(){
-
+    image(video, 10, 10, 330, 270);
 }
 
 function take_snapshot() {
