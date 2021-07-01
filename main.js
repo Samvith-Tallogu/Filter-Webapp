f1108954c0d2f26907c0364ad682780ec7b8f1de
Samvith-Tallogu <77870783+Samvith-Tallogu@users.noreply.github.com>
2021-07-01 15:19:20 +0530

    Webapp Completed

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
-function preload(){}
+
+nosex = 0;
+nosey = 0;
+function preload(){
+    mous = loadImage('https://i.postimg.cc/Yqc5MVBV/mts.png');
+}
 
 function setup(){
     canvas = createCanvas(350 , 300);
@@ -9,21 +14,30 @@ function setup(){
     poseNet = ml5.poseNet(video, modelLoaded);
     poseNet.on('pose', gotPoses);
 }
-function modelLoaded() {
-    console.log("Model Loaded");
-}
+
 function gotPoses(results) {
     if (results.length > 0) {
         console.log(results);
-        console.log('nose X = '+ results[0].pose.nose.x);
-        console.log('nose Y = '+ results[0].pose.nose.y);
+        nosex = results[0].pose.nose.x - 38;
+        nosey = results[0].pose.nose.y - 1;
+        console.log('nose X = '+ nosex);
+        console.log('nose Y = '+ nosey);
     }
 }
 
+function modelLoaded() {
+    console.log("Model Loaded");
+}
+
+
 function draw(){
-    image(video, 10, 10, 330, 270);
+    image(video, 0, 0, 350, 300);
+   // circle(nosex, nosey, 20);
+    //stroke(255, 0, 0);
+    //fill(255, 0, 0);
+    image(mous, nosex, nosey, 80, 40);
 }
 
 function take_snapshot() {
-    save("me_with_clown_nose.png");
+    save("me_with_moustache.png");
 }
